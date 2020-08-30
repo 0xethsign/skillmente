cdd83e017267ef8de15eee9592ec3aff309cb4e2
smrpn <samarpan_d@pp.iitr.ac.in>
2020-08-30 20:42:55 +0530

    ???

--- presite/settings.py
+++ presite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g@vlaj!4)6i15+hy@z=c@q(ug54&@i)+)t*ogs=3^nd0je*5z9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/base/static'),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, 'static')
+
 ]
 
 

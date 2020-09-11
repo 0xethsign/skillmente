a5fd74db50d0691497e51d1902b581fc84e2294a
smrpn <samarpan_d@pp.iitr.ac.in>
2020-09-12 03:16:10 +0530

    mobile version

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from .views import AddEmail, AddTutor, Index
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('', AddEmail.as_view(), name="cover"),
     path('careers/', AddTutor.as_view(), name="careers"),
-    # path('cover/', Index.as_view(), name="cover"),
+    # path('favicon.ico', RedirectView.as_view(
+    #     url=staticfiles_storage.url('res/favicon.ico')))
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
